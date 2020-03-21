e3688864af754f3809233c04bff9a02fef0daf29
lpraz <lpraz@mts.net>
2020-03-21 09:04:16 -0500

    Added bingo card viewing

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 import {h, app} from "hyperapp"
 
 const minSpaces = 24;
-const states = {editor: "editor"};
+const states = {editing: "editing", viewing: "viewing"};
 
 const eventValue = event => event.target.value;
 
 const view = {
     main: state => {
         console.log(state);
-        if (state.state == states.editor)
-            return view.editor(state.contents);
-        else
-            return view.error();
+        if (state.state == states.editing)
+            return view.editor(state);
+        
+        if (state.state == states.viewing)
+            return view.viewer(state);
+        
+        return view.error();
     },
 
     error: () => "An unexpected error occurred! Please refresh and try again.",
 
     editor: state => h("div", {}, [
-        view.freeSpace(state.freeSpace),
-        view.spaces(state.spaces),
+        view.title(state.contents.title),
+        view.freeSpace(state.contents.freeSpace),
         view.addSpace(),
-        view.generate()
+        view.generate(),
+        view.spaces(state.contents.spaces)
+    ]),
+
+    title: title => h("div", {}, [
+        h("label", {}, "Title:"),
+        h("input", {onChange: [action.editTitle, eventValue],
+            value: title})
     ]),
 
     freeSpace: freeSpace => h("div", {}, [
         h("label", {}, "Free space:"),
-        h("input", {}, freeSpace)
+        h("input", {
+            onChange: [action.editFreeSpace, eventValue],
+            value: freeSpace})
     ]),
 
     spaces: spaces => spaces.map((item, index) => h("div", {}, [
@@ -33,7 +45,8 @@ const view = {
             "button",
             {
                 disabled: spaces.length === minSpaces,
-                onClick: action.removeSpace(index)
+                onClick: action.removeSpace(index),
+                tabindex: -1
             },
             "-"),
         h("label", {}, `${index + 1}:`),
@@ -42,10 +55,52 @@ const view = {
 
     addSpace: () => h("button", {onClick: action.addSpace}, "+"),
 
-    generate: () => h("button", {}, "Generate!")
+    generate: () => h("button", {onClick: action.changeToView}, "Generate!"),
+
+    viewer: state => h("div", {}, [
+        h("table", {}, [
+            view.cardTitle(state.contents.title),
+            view.cardRow(state.contents.spaces.slice(0, 5)),
+            view.cardRow(state.contents.spaces.slice(5, 10)),
+            view.cardFreeSpaceRow(state.contents.spaces.slice(10, 14),
+                state.contents.freeSpace),
+            view.cardRow(state.contents.spaces.slice(14, 19)),
+            view.cardRow(state.contents.spaces.slice(19, 24))
+        ])
+    ]),
+
+    cardTitle: title => h("tr", {}, h("th", {colspan: 5}, title)),
+
+    cardRow: spaces => h("tr", {}, spaces.map(view.cardSpace)),
+
+    cardFreeSpaceRow: (spaces, freeSpace) => h("tr", {}, [
+        view.cardSpace(spaces[0]),
+        view.cardSpace(spaces[1]),
+        view.cardSpace(freeSpace),
+        view.cardSpace(spaces[2]),
+        view.cardSpace(spaces[3])
+    ]),
+
+    cardSpace: space => h("td", {}, space)
 };
 
 const action = {
+    editTitle: (state, newTitle) => ({
+        ...state,
+        contents: {
+            ...state.contents,
+            title: newTitle
+        }
+    }),
+
+    editFreeSpace: (state, newSpace) => ({
+        ...state,
+        contents: {
+            ...state.contents,
+            freeSpace: newSpace
+        }
+    }),
+
     addSpace: state => ({
         ...state,
         contents: {
@@ -74,14 +129,20 @@ const action = {
             spaces: state.contents.spaces.filter(
                 (_, index) => index != removeIndex)
         }
+    }),
+
+    changeToView: state => ({
+        ...state,
+        state: states.viewing
     })
 };
 
 app({
     node: document.getElementById("app"),
     init: {
-        state: states.editor,
+        state: states.editing,
         contents: {
+            title: "",
             freeSpace: "",
             spaces: Array(minSpaces).fill(undefined)
         }
